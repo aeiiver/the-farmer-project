bf4d14cb117a861f14e2757a5d6accec8786fc09
alexmario5 <pineau.alex@gmail.com>
2022-12-12 10:47:31 +0100

    Javadoc des Tests Data

--- src/test/java/root/data/AdminDaoTest.java
+++ src/test/java/root/data/AdminDaoTest.java
@@ -6,22 +6,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AdminDaoTest {
 
+  /**
+   * Ce test doit vérifier que l'insertion des données dans
+   * la table Admin renvoie bien true s'ils sont bien envoyés.
+   */
   @Test
   void insert() {
   }
 
+  /**
+   * Ce test doit vérifier si la récupération d'une ligne de données
+   * dans la table Admin renvoie bien true s'ils sont bien reçus.
+   */
   @Test
   void get() {
   }
 
+  /**
+   * Ce test doit vérifier si la récupération de toutes les données
+   * dans la table Admin renvoie bien true s'ils sont bien reçus.
+   */
   @Test
   void getAll() {
   }
 
+  /**
+   * Ce test doit vérifier que la mise à jour des données dans
+   * la table Admin renvoie bien true s'ils sont bien modifiés.
+   */
   @Test
   void update() {
   }
 
+  /**
+   * Ce test doit vérifier que la suppresion des données dans
+   * la table Admin renvoie bien true s'ils sont bien enlevés.
+   */
   @Test
   void delete() {
   }
