abd8288f3582098f93fc0b765df87277e3edf0b3
aeiiver <101661283+aeiiver@users.noreply.github.com>
2022-12-13 13:38:07 +0100

    Ajoute tests de AdminDao
    Squash me wit hthe below
    
    squash me onto the one below
    
    squash onto  below me

--- src/test/java/root/data/AdminDaoTest.java
+++ src/test/java/root/data/AdminDaoTest.java
@@ -1,17 +1,60 @@
 package root.data;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import root.model.Admin;
+
 class AdminDaoTest {
+
+  static Connection connexion;
+  static Statement statement;
+  static AdminDao systemUnderTest;
+
+  @BeforeAll
+  static void setup() throws ClassNotFoundException, SQLException {
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    connexion = DriverManager.getConnection(
+        System.getenv("DB_URL_DEV"),
+        System.getenv("DB_USER"),
+        System.getenv("DB_PSW")
+    );
+    statement = connexion.createStatement();
+    systemUnderTest = new AdminDao(connexion);
+  }
+
+  @AfterAll
+  static void teardown() throws SQLException {
+    statement.close();
+    connexion.close();
+  }
+
+  @AfterEach
+  void teardownEach() throws SQLException {
+    statement.executeUpdate("DELETE FROM Admin;");
+  }
 
   /**
    * Ce test doit vérifier que l'insertion des données dans
    * la table Admin renvoie bien true s'ils sont bien envoyés.
    */
   @Test
-  void insert() {
+  void insert() throws SQLException {
+    Admin admin = new Admin("testemail@dontmail.me", "1234hasher_moi", -1, "John");
+
+    boolean recu = systemUnderTest.insert(admin);
+
+    assertTrue(recu);
+    assertEquals(1, TestUtil.nombreLignesTrouvees(statement, "Admin"));
   }
 
   /**
@@ -19,7 +62,16 @@ class AdminDaoTest {
    * dans la table Admin renvoie bien true s'ils sont bien reçus.
    */
   @Test
-  void get() {
+  void get() throws SQLException {
+    statement.executeUpdate("INSERT INTO Admin VALUES "
+        + "(42, 'testemail@dontmail.me', 'Jean', '1234hasher_moi');");
+
+    Admin recu = systemUnderTest.get("Jean");
+
+    assertEquals(42, recu.getIdAdmin());
+    assertEquals("testemail@dontmail.me", recu.getMail());
+    assertEquals("Jean", recu.getPseudo());
+    assertEquals("1234hasher_moi", recu.getMdp());
   }
 
   /**
@@ -27,7 +79,33 @@ class AdminDaoTest {
    * dans la table Admin renvoie bien true s'ils sont bien reçus.
    */
   @Test
-  void getAll() {
+  void getAll() throws SQLException {
+    List<Admin> adminsAttendus = List.of(
+        new Admin("testemail@dontmail.me", "1234hasher_moi", 42, "John"),
+        new Admin("trying.something@website.org", "un_mot_de_passe", 43, "Jean"),
+        new Admin("blabla@abc.xyz", "le-chat-est-passe-par-la", 44, "Carl"));
+    connexion.setAutoCommit(false);
+    for (Admin admin : adminsAttendus) {
+      statement.addBatch(String.format("INSERT INTO Admin VALUES (%d, '%s', '%s', '%s');",
+          admin.getIdAdmin(), admin.getMail(), admin.getPseudo(), admin.getMdp()));
+    }
+    statement.executeBatch();
+    statement.clearBatch();
+    connexion.commit();
+    connexion.setAutoCommit(true);
+
+    ArrayList<Admin> adminsRecus = systemUnderTest.getAll();
+
+    assertTrue(adminsRecus.size() == adminsAttendus.size());
+    for (int index = 0; index < adminsAttendus.size(); ++index) {
+      Admin attendu = adminsAttendus.get(index);
+      Admin recu = adminsRecus.get(index);
+
+      assertEquals(attendu.getIdAdmin(), recu.getIdAdmin());
+      assertEquals(attendu.getMail(), recu.getMail());
+      assertEquals(attendu.getPseudo(), recu.getPseudo());
+      assertEquals(attendu.getMdp(), recu.getMdp());
+    }
   }
 
   /**
@@ -35,7 +113,16 @@ class AdminDaoTest {
    * la table Admin renvoie bien true s'ils sont bien modifiés.
    */
   @Test
-  void update() {
+  void update() throws SQLException {
+    Admin admin = new Admin("testemail@dontmail.me", "1234hasher_moi", -1, "John");
+    statement.executeUpdate(String.format("INSERT INTO Admin VALUES (%d, '%s', '%s', '%s')",
+        admin.getIdAdmin(), admin.getMail(), admin.getPseudo(), admin.getMdp()));
+    admin.setMail("jai.change.de.mail@mail.me");
+    admin.setMdp("mot-de-passe-en-clair");
+
+    boolean recu = systemUnderTest.update(admin);
+
+    assertTrue(recu);
   }
 
   /**
@@ -43,6 +130,15 @@ class AdminDaoTest {
    * la table Admin renvoie bien true s'ils sont bien enlevés.
    */
   @Test
-  void delete() {
+  void delete() throws SQLException {
+    Admin admin = new Admin("testemail@dontmail.me", "1234hasher_moi", -1, "John");
+    statement.executeUpdate(String.format("INSERT INTO Admin VALUES (%d, '%s', '%s', '%s')",
+        admin.getIdAdmin(), admin.getMail(), admin.getPseudo(), admin.getMdp()));
+
+    boolean recu = systemUnderTest.delete(admin);
+
+    assertTrue(recu);
+    assertEquals(0, TestUtil.nombreLignesTrouvees(statement, "Admin"));
   }
+
 }
